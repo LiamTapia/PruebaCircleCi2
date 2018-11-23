ec154fb45b45a8d6edbb3c1e853d1477d7342fe1
LiamTapia <Plasticolino1>
2018-11-23 09:56:49 -0800

    Prueba CircleCi

---
+++ src/main/java/Liamsin/TestCircleCi/App.java
@@ -0,0 +1,13 @@
+package Liamsin.TestCircleCi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
